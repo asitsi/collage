791e5827d9c3302c68139a8456a308121c1b12a6
asitsi <41507591+asitsi@users.noreply.github.com>
2021-06-12 16:42:45 +0530

    'collage'

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Singhup from "./Pages/SignUp";
+import Singhin from "./Pages/Signin";
+import Home from "./Pages/Home";
+import AdminRoute from "./Pages/helper/AdminRouts";
+import AdminDashBoard from "./Pages/AdminDashboard";
+import Head from "./Pages/Head";
+import Teacher from "./Pages/Teacher";
+import Student from "./Pages/Student";
+import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Switch>
+        <Route path="/signup" exact component={Singhup} />
+        <Route path="/signin" exact component={Singhin} />
+        <AdminRoute path="/admin/dashboard" exact component={AdminDashBoard} />
+        <Route path="/dashboard/:HeadId" exact component={Head} />
+        <Route path="/dashboard/:HeadId/:Teacher" exact component={Teacher} />
+        <Route
+          path="/dashboard/:HeadId/:Teacher/:student"
+          exact
+          component={Student}
+        />
+        <Route path="/" exact component={Home} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
